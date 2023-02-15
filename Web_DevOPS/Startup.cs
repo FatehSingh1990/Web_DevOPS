e75f08e4fd81980e5c35a79002a70a937ae11c1a
FatehSingh <fathsingh1990@gmail.com>
2023-02-15 13:57:35 +0530

    First

---
+++ Web_DevOPS/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Web_DevOPS.Startup))]
+namespace Web_DevOPS
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
